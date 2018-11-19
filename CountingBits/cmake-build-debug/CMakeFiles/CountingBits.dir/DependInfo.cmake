12c2d59e52bf8f6bef71e88621ebf85215e169b4
hanlei <yyhanlei@gmail.com>
2018-11-19 23:21:23 +0800

    update

--- CountingBits/cmake-build-debug/CMakeFiles/CountingBits.dir/DependInfo.cmake
+++ CountingBits/cmake-build-debug/CMakeFiles/CountingBits.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/halxp/Desktop/C++/CountingBits/main.cpp" "/Users/halxp/Desktop/C++/CountingBits/cmake-build-debug/CMakeFiles/CountingBits.dir/main.cpp.o"
+  "/Users/halxp/Desktop/AlgorithmCode/CountingBits/main.cpp" "/Users/halxp/Desktop/AlgorithmCode/CountingBits/cmake-build-debug/CMakeFiles/CountingBits.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
--- CountingBits/cmake-build-debug/cmake_install.cmake
+++ CountingBits/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/halxp/Desktop/C++/CountingBits
+# Install script for directory: /Users/halxp/Desktop/AlgorithmCode/CountingBits
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/halxp/Desktop/C++/CountingBits/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/halxp/Desktop/AlgorithmCode/CountingBits/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
